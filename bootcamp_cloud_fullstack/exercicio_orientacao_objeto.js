1dc72d326da85b4e6e81c447cc031e205d2f47c0
pacasarini <paulacasarini@hotmail.com>
2022-10-22 01:19:16 -0300

    enunciado do exercicio

---
+++ bootcamp_cloud_fullstack/exercicio_orientacao_objeto.js
@@ -0,0 +1,7 @@
+/*
+crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por KM rodado.
+Crie um método que dado a quantidade de KM e o preço de combustível nos dê o
+valor gasto em reais para realizar este percurso.
+*/
+
