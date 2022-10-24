37e881e2fb7b88224beda963c485439d837e5895
pacasarini <paulacasarini@hotmail.com>
2022-10-24 01:10:28 -0300

    fim do exercicio proposto

--- bootcamp_cloud_fullstack/exercicio_objeto_imc.js
+++ bootcamp_cloud_fullstack/exercicio_objeto_imc.js
@@ -18,11 +18,41 @@ class Pessoa {
         this.altura = altura;
     }
 
-    //METODO:
+    //METODO para calcular:
     calcularIMC(){   //só colocar os parametros entre parenteses se precisar de infos externas a pessoa
-        return this.peso / this.peso ** 2;   //como tudo o q precisa pro calculo esta dentro da classe, nao eh preciso colocar nada no parenteses do metodo
+        return this.peso / (this.altura * this.altura);   //como tudo o q precisa pro calculo esta dentro da classe, nao eh preciso colocar nada no parenteses do metodo
     }
+
+    //METODO para classificar:
+    classificarIMC(){
+        const imc = this.calcularIMC();  //Para classificar o IMC: colocar o metodo classificarIMC dentro da classe
+        if (imc < 18.5){
+            return ('Abaixo do peso');
+        } else if(imc >= 18.5 && imc < 25) {
+            return ('Peso normal');
+        } else if (imc >= 25 && imc < 40) {
+            return ('Obeso');
+        } else {
+            return ('Obesidade grave');
+        }
+    }
+
+}
+
+function imprimirIMC(pessoa) {
+    console.log(`Pessoa: ${pessoa.nome}; Peso: ${pessoa.peso}; Classificação: ${pessoa.classificarIMC()}`);
 }
 
 const jose = new Pessoa('José', 70, 1.75);
-console.log(jose)
+
+const renan = new Pessoa('Renan', 63, 1.75);
+
+const vitor = new Pessoa('Vitor', 60, 1.69);
+
+const michel = new Pessoa('Michel', 110, 1.67);
+
+
+
+
+console.log(imprimirIMC(renan));
+
