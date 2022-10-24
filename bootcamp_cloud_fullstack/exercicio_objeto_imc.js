5f0894cb6da3ebf580ce6c256fc31efda946b080
pacasarini <paulacasarini@hotmail.com>
2022-10-24 00:08:31 -0300

    exercicio objeto - IMC

---
+++ bootcamp_cloud_fullstack/exercicio_objeto_imc.js
@@ -0,0 +1,8 @@
+/*
+Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu iMC (IMC = peso / (altura * altura));
+instancie uma pessoa chamada Jose que tenha 70kg de peso e 1.75 m de altura e peça ao Jose para
+dizer o valor do seu IMC
+*/
+
