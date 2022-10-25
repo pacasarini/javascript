b859bed86de3c6d5e8e80e07f6cd9b7e1563c3e1
pacasarini <paulacasarini@hotmail.com>
2022-10-25 00:11:52 -0300

    com destruction

--- bootcamp_cloud_fullstack/exercicio_importacoes/main.js
+++ bootcamp_cloud_fullstack/exercicio_importacoes/main.js
@@ -1,3 +1,8 @@
-const funcoes = require('./funcoes_auxiliares');
+/* const funcoes = require('./funcoes_auxiliares');
 
-console.log(funcoes.gets());
+console.log(funcoes.gets());  */
+
+//da forma abaixo eh mais avançado e podemos ter varios codigos dentro de 1:
+const { gets, print } = require('./funcoes_auxiliares');
+
+print(gets());
