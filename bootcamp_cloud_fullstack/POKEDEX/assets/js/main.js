ec50117bba57250ff355612d87007440bcbdd5be
pacasarini <paulacasarini@hotmail.com>
2022-11-05 02:35:23 -0300

    manipulating the list

--- bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
+++ bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
@@ -1,6 +1,30 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+    
+            <div class="detail">
+            
+                <ol class="types">  
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>  
+                
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+
+            </div>     
+
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList') 
+    //estou pegando a lista de pokemon e atribuindo a um obj pra ser manipulado
+    // "document" se refere a pagina inteira
+
+
 
 /*fetch(url)   //requisicao / promisse - funciona como o try/catch/finally
     .then(function (response) {     //sucesso
@@ -19,16 +43,17 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 Pra nao ficar call back dentro de call back, usamos o retorno da promise como resposta
 com o comando return.response.json() para reacao em cascata, evitando assim uma serie de callback*/
 
-
-
 //arrow function (=>) substitui a funcao declarada (function).
 //se tiver soh 1 linha dessa funcao, nao precisa declarar o corpo, entao pode suprimir uma boa parte 
-//podendo simplificar e ficando assim:
 
-fetch(url)   
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) = console.error(error)) 
-    // o finally foi suprimido pq nao vamos usa-lo agora
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')
+
+    //o metodo .map substitui o comando abaixo:
+    // for (let i = 0; i < pokemons.length; i++) {
+    //     const pokemon = pokemons[i];
+    //     listItems.push(convertPokemonToLi(pokemon))
+    // }
+})
 
     
