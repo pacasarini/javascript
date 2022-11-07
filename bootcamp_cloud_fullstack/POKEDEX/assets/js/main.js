984fe71bf1450c94d5675d81a45aca0de43624cc
pacasarini <paulacasarini@hotmail.com>
2022-11-07 00:15:06 -0300

    code review

--- bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
+++ bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
@@ -1,18 +1,23 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>` )
+}
+
+ 
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}#</span>
             <span class="name">${pokemon.name}</span>
     
             <div class="detail">
             
                 <ol class="types">  
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>  
                 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
 
             </div>     
 
--- bootcamp_cloud_fullstack/POKEDEX/assets/js/poke-api.js
+++ bootcamp_cloud_fullstack/POKEDEX/assets/js/poke-api.js
@@ -1,11 +1,17 @@
 
 const pokeApi = {}  
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-    }
-   
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
