94b4820f53a99eabca1a94062743c38f6cebb0ae
pacasarini <paulacasarini@hotmail.com>
2022-11-07 02:22:01 -0300

    pagination

--- bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
+++ bootcamp_cloud_fullstack/POKEDEX/assets/js/main.js
@@ -1,57 +1,47 @@
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">${pokemon.number}#</span>
-            <span class="name">${pokemon.name}</span>
-    
-            <div class="detail">
-                <ol class="types">  
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>  
-                
-                <img src="${pokemon.photo}" 
-                    alt="${pokemon.name}">
-            </div>     
-        </li>
-    `
-}
-
 const pokemonList = document.getElementById('pokemonList') 
-    //estou pegando a lista de pokemon e atribuindo a um obj pra ser manipulado
-    // "document" se refere a pagina inteira
+//estou pegando a lista de pokemon e atribuindo a um obj pra ser manipulado
+// "document" se refere a pagina inteira
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 10
+let offset = 0;
 
 
+function loadPokemonItens(offset, limit) {
+function convertPokemonToLi(pokemon) {
+        return 
+    }
 
-/*fetch(url)   //requisicao / promisse - funciona como o try/catch/finally
-    .then(function (response) {     //sucesso
-        //console.log(response)    ----- ver abaixo a explicacao*      
-        return response.json()      //esse comando retorna a promessa do body em json
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">${pokemon.number}#</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="detail">
+                    <ol class="types">  
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>  
+                    
+                    <img src="${pokemon.photo}" 
+                        alt="${pokemon.name}">
+                </div>     
+            </li>
+    `).join('')
+        pokemonList.innerHTML += newHtml
     })
-    .catch(function (error) {       //fracasso
-        console.error(error)
-    }) 
-    .finally(function (){          //concluida, independente do sucesso ou fracasso
-        console.log('requisicao concluida')
-    }) */
-
-
-/*o body vem como "Readable Stream" e precisa ser transformado em JSON para poder manipula-lo
-Pra nao ficar call back dentro de call back, usamos o retorno da promise como resposta
-com o comando return.response.json() para reacao em cascata, evitando assim uma serie de callback*/
-
-//arrow function (=>) substitui a funcao declarada (function).
-//se tiver soh 1 linha dessa funcao, nao precisa declarar o corpo, entao pode suprimir uma boa parte 
-
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml
-
+}
     //o metodo .map substitui o comando abaixo:
     // for (let i = 0; i < pokemons.length; i++) {
     //     const pokemon = pokemons[i];
     //     listItems.push(convertPokemonToLi(pokemon))
     // }
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
 
     
--- bootcamp_cloud_fullstack/POKEDEX/assets/js/poke-api.js
+++ bootcamp_cloud_fullstack/POKEDEX/assets/js/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
             .then(convertPokeApiDetailToPokemon) 
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -33,3 +33,23 @@ pokeApi.getPokemons = (offset = 0, limit = 20) => {
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
+/*fetch(url)   //requisicao / promisse - funciona como o try/catch/finally
+    .then(function (response) {     //sucesso
+        //console.log(response)    ----- ver abaixo a explicacao*      
+        return response.json()      //esse comando retorna a promessa do body em json
+    })
+    .catch(function (error) {       //fracasso
+        console.error(error)
+    }) 
+    .finally(function (){          //concluida, independente do sucesso ou fracasso
+        console.log('requisicao concluida')
+    }) */
+
+    /*o body vem como "Readable Stream" e precisa ser transformado em JSON para poder manipula-lo
+Pra nao ficar call back dentro de call back, usamos o retorno da promise como resposta
+com o comando return.response.json() para reacao em cascata, evitando assim uma serie de callback*/
+
+//arrow function (=>) substitui a funcao declarada (function).
+//se tiver soh 1 linha dessa funcao, nao precisa declarar o corpo, entao pode suprimir uma boa parte 
